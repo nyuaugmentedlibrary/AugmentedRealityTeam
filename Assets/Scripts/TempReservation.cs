56031f0385f66195f5b6f824ac46e56d90b56cbd
ShadowCat567 <lv2067@nyu.edu>
2023-04-25 20:47:05 -0400

    Temp room reservation system fully implemeted

--- Assets/Scripts/TempReservation.cs
+++ Assets/Scripts/TempReservation.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TempReservation : MonoBehaviour
 {
-    bool reserved = true;
+    bool reserved = false;
     [SerializeField] GameObject[] walls = new GameObject[5];
+    [SerializeField] TMP_Text resButtonTxt;
+    [SerializeField] int roomNum;
 
     Color openColor = Color.green;
     Color resColor = Color.red;
@@ -13,14 +16,37 @@ public class TempReservation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < walls.Length; i ++)
+        resButtonTxt.text = roomNum + " is not reserved";
+
+        for (int i = 0; i < walls.Length; i ++)
         {
-            walls[i].GetComponent<Material>().color = openColor;
+            walls[i].GetComponent<Renderer>().materials[0].color = openColor;
         }
     }
 
     public void ReserveRoom()
     {
+        if(!reserved)
+        {
+            reserved = true;
 
+            for (int i = 0; i < walls.Length; i++)
+            {
+                walls[i].GetComponent<Renderer>().materials[0].color = resColor;
+            }
+
+            resButtonTxt.text = roomNum + " is reserved";
+        }
+        else
+        {
+            reserved = false;
+
+            for (int i = 0; i < walls.Length; i++)
+            {
+                walls[i].GetComponent<Renderer>().materials[0].color = openColor;
+            }
+
+            resButtonTxt.text = roomNum + " is not reserved";
+        }
     }
 }
